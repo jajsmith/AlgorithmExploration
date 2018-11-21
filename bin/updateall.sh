07d27debb2483299abdd9ba815efede3d795e0b2
Jonathan Smith <j.jajsmith@gmail.com>
2018-11-21 11:44:26 -0500

    updateall improvements
    - update summary
    - better error handling and messaging
    - switch away from cactus branching model

--- bin/updateall.sh
+++ bin/updateall.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 # Update All
 # by Jonathan Smith
@@ -14,33 +14,38 @@ RED_C='\033[0;31m'
 GREEN_C='\033[0;32m'
 CYAN_C='\033[0;36m'
 YELLOW_C='\033[0;33m'
-NO_C='\033[0m'
-
-# Other Constants
+NO_C='\033[0m' # Other Constants
 PKGLCK_GIT_OUT=' M package-lock.json'
 PKGLCK_NAME='package-lock.json'
 
 # Functions
 log_action() {
-	echo "${CYAN_C}> ${1}${NO_C}"
+	echo -e "${CYAN_C}> ${1}${NO_C}"
 }
 log_warn() {
-	echo "${YELLOW_C}> WARN: $1${NO_C}"
+	echo -e "${YELLOW_C}> WARN: $1${NO_C}"
 }
 log_error() {
-	echo "${RED_C}> ERROR: $1${NO_C}"
+	echo -e "${RED_C}> ERROR: $1${NO_C}"
 }
 log_success() {
-	echo "${GREEN_C}$1${NO_C}"
+	echo -e "${GREEN_C}$1${NO_C}"
 }
 
-DIR=`pwd`
-REPOS=($@)
-echo "Updating the following repositories:"
-echo ${REPOS[*]}
+if [[ -f "$1" ]]; then
+	log_action "Converting file to repositories..."
+	REPOS=(`cat $1`)
+else
+	REPOS=($@)
+fi
 
+log_success "Updating the following repositories:"
+log_success "${REPOS[*]}"
+DIR=`pwd` REPO_STATUS=("${REPOS[@]}")
+REPO_COUNT=0
 for REPO in ${REPOS[@]}; do
-	echo "\n>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"
+	STATUS="updating"
+	echo -e "\n"
 	log_action "== Updating ${REPO} =="
 	cd ${DIR}/${REPO}
 	GIT_LOG="git-pull.log"
@@ -49,6 +54,10 @@ for REPO in ${REPOS[@]}; do
 	if [[ $(git branch | grep '*') != "* master" ]]; then
 		log_error "Not in master!"
 	  log_warn "SKIPPED."
+		# Status
+		STATUS="skipped not in master"
+		REPO_STATUS[$REPO_COUNT]="${STATUS}"
+		let REPO_COUNT+=1;
 		continue
 	fi
 
@@ -59,23 +68,43 @@ for REPO in ${REPOS[@]}; do
 		git checkout $PKGLCK_NAME
 	fi
 
-	echo "${CYAN_C}> git pull upstream master${NO_C}"
-	git pull upstream master | tee $GIT_LOG
+	echo -e "${CYAN_C}> git pull origin master${NO_C}"
+	# Writes both stdout and stderr to $GIT_LOG as well as terminal output
+	git pull origin master > >(tee -a $GIT_LOG) 2> >(tee -a $GIT_LOG)
 
+	echo `grep -q "Aborting" $GIT_LOG`
+
+	STATUS="updated"
 	if grep -q "Already up-to-date" $GIT_LOG; then
-		log_success "Up-to-date, skipping rest of instructions..."
-	else	
-		log_action "git push origin master"
-		git push origin master
-
-		log_action "npm i -q"
-		npm i -q
+		log_success "== Up-to-date, skipping rest of instructions..."
+		STATUS="already up-to-date"
+	elif grep -q "Aborting" $GIT_LOG; then
+		log_error "Aborted git command for ${REPO}..."
+		STATUS="error updating"
+	elif grep -q "error" $GIT_LOG; then
+		log_error "Error found in git command for ${REPO}..."
+		STATUS="error updating"
+#	else	
+#		log_action "git push origin master"
+#		git push origin master
+#
+#		log_action "npm i -q"
+#		npm i -q
+	else
+		log_success "== UPDATE [${REPO}] COMPLETE"
 	fi
 
-	log_success "== UPDATE [${REPO}] COMPLETE =="
 
 	# Cleanup
 	rm $GIT_LOG
+
+	# Status
+	REPO_STATUS[$REPO_COUNT]="${STATUS}"
+	let REPO_COUNT+=1;
 done
 
+echo -e "\nRepository update summary:"
+for (( i=0; i<${#REPOS[@]}; i++)); do
+	printf "%25s %25s\n" "${REPOS[$i]}" "${REPO_STATUS[$i]}"
+done
 
